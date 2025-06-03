12d65751d50e9431f534b6b860e1dded1044c9d5
Juliana <juhfreddi@gmail.com>
2025-06-03 16:46:44 -0300

    Segundo commit

--- Menu.ts
+++ Menu.ts
@@ -6,7 +6,6 @@ export function main() {
 
     let opcao: number;
 
-    // Objeto da Classe Conta (Teste)
     const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
     conta.visualizar();
     conta.sacar(10500);
